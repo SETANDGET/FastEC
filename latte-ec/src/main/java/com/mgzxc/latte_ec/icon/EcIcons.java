4cd4ad2359720afddc141a4bf07a09ad6deb6bb8
mgzxc <2456541083@qq.com>
2018-05-07 21:35:13 +0800

    init

---
+++ latte-ec/src/main/java/com/mgzxc/latte_ec/icon/EcIcons.java
@@ -0,0 +1,20 @@
+package com.mgzxc.latte_ec.icon;
+
+import com.joanzapata.iconify.Icon;
+
+/**
+ * Created by MG_ZXC on 2018/5/7.
+ */
+public enum EcIcons implements Icon{
+    ;
+
+    @Override
+    public String key() {
+        return null;
+    }
+
+    @Override
+    public char character() {
+        return 0;
+    }
+}
