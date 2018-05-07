ff4bbca411086b953d87851a3b55efa525340c87
mgzxc <2456541083@qq.com>
2018-05-07 22:26:19 +0800

    init

---
+++ latte-core/src/main/java/com/mgzxc/latte_core/activities/ProxyActivity.java
@@ -0,0 +1,10 @@
+package com.mgzxc.latte_core.activities;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by MG_ZXC on 2018/5/7.
+ */
+public class ProxyActivity extends SupportActivity{
+
+}
