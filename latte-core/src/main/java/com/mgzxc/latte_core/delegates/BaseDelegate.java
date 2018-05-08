604dea07faa6a1de1bde0086cab8b013ca4a90fa
mgzxc <2456541083@qq.com>
2018-05-08 13:49:48 +0800

    基础delegate

--- latte-core/src/main/java/com/mgzxc/latte_core/delegates/BaseDelegate.java
+++ latte-core/src/main/java/com/mgzxc/latte_core/delegates/BaseDelegate.java
@@ -6,13 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import me.yokeyword.fragmentation_swipeback.SwipeBackFragment;
 
 /**
  * Created by MG_ZXC on 2018/5/8.
  */
 public abstract class BaseDelegate extends SwipeBackFragment{
-    public abstract Object setLayout();
+    @SuppressWarnings("SpellCheckingInspection")
+    private Unbinder mUnbinder = null;
+    public abstract Object setLayout();//子类提供布局视图
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -24,8 +28,19 @@ public abstract class BaseDelegate extends SwipeBackFragment{
         }
         //绑定View
         if (rootView!=null){
-
+            mUnbinder = ButterKnife.bind(this, rootView);
+            onBindView(savedInstanceState, rootView);
         }
         return rootView;
+    }
+
+    protected abstract void onBindView(@Nullable Bundle savedInstanceState, View rootView);
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mUnbinder!=null) {
+            mUnbinder.unbind();//解除绑定
+        }
     }
 }
