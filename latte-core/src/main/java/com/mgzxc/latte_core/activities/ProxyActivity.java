4b30db686bbab89095e5717fcf83cfa1483b1c2f
mgzxc <2456541083@qq.com>
2018-05-08 14:05:04 +0800

    基础delegate

--- latte-core/src/main/java/com/mgzxc/latte_core/activities/ProxyActivity.java
+++ latte-core/src/main/java/com/mgzxc/latte_core/activities/ProxyActivity.java
@@ -1,11 +1,38 @@
 package com.mgzxc.latte_core.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.ContentFrameLayout;
+
+import com.mgzxc.latte_core.R;
+import com.mgzxc.latte_core.delegates.LatteDelegae;
+
 import me.yokeyword.fragmentation.SupportActivity;
 
 /**
  * Created by MG_ZXC on 2018/5/7.
  */
-public class ProxyActivity extends SupportActivity{
+public abstract class ProxyActivity extends SupportActivity{
+    public abstract LatteDelegae setRootDelegate();//设置根fragment
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initContainer(savedInstanceState);
+    }
+    public void initContainer(@Nullable Bundle savedInstanceState){
+        ContentFrameLayout container = new ContentFrameLayout(this);
+        container.setId(R.id.delegate_container);
+        setContentView(container);
+        if (savedInstanceState==null) {
+            loadRootFragment(R.id.delegate_container, setRootDelegate());//加载Delegate
+        }
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.gc();
+        System.runFinalization();
+    }
 }
