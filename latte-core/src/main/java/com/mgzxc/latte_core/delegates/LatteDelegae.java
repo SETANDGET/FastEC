a19a00f3443a7f30cdf23a8ffb6565ff8934b96e
mgzxc <2456541083@qq.com>
2018-05-08 13:53:45 +0800

    基础delegate

---
+++ latte-core/src/main/java/com/mgzxc/latte_core/delegates/LatteDelegae.java
@@ -0,0 +1,7 @@
+package com.mgzxc.latte_core.delegates;
+
+/**
+ * Created by MG_ZXC on 2018/5/8.
+ */
+public abstract class LatteDelegae extends PermissionCheckerDelegate{
+}
---
+++ latte-core/src/main/java/com/mgzxc/latte_core/delegates/PermissionCheckerDelegate.java
@@ -0,0 +1,8 @@
+package com.mgzxc.latte_core.delegates;
+
+/**
+ * Created by MG_ZXC on 2018/5/8.
+ * 中间层 权限的判定
+ */
+public abstract class PermissionCheckerDelegate extends BaseDelegate{
+}
